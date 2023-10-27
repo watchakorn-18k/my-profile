13083288a6f18beb08f55cce43c38b7e04047585
user.name <wk18k@proton.me>
2023-10-28 01:02:09 +0700

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    import "../app.css";
+</script>
+
+<div
+    class="navbar
+bg-base-200 text-base-content sticky top-0 z-30 flex w-full justify-center bg-opacity-90 backdrop-blur transition-all duration-100 [transform:translate3d(0,0,0)]
+shadow-sm font-mono
+"
+>
+    <div class="flex-1">
+        <a class="btn btn-ghost normal-case text-xl" href="/">Home</a>
+    </div>
+    <div class="flex-none">
+        <ul class="menu menu-horizontal px-1">
+            <li><a href="/about">About</a></li>
+        </ul>
+    </div>
+</div>
+
+<slot />
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+</script>
+
+<div class="text-center font-mono mt-14">
+    <h1 class="text-3xl">🤳🏻My Profile</h1>
+    <div class="mx-24 flex flex-col gap-16 items-center mt-6">
+        <img
+            class="w-[30rem] lg:w-[15rem]"
+            src="https://github.com/watchakorn-18k/watchakorn-18k/raw/main/devcard.svg"
+            alt="metrics"
+        />
+        <img
+            class="w-[40rem] lg:w-[40rem]"
+            src="https://github.com/watchakorn-18k/watchakorn-18k/raw/main/github-metrics.svg"
+            alt="metrics"
+        />
+    </div>
+</div>
