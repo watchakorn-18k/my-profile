6ec8587cb4326b8f7b9f78f27122fe4a0ff1e011
watchakorn-18k <wk18k@proton.me>
2024-02-25 03:43:24 +0700

    docs : add new porject

--- src/routes/portfolio/+page.svelte
+++ src/routes/portfolio/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import users from "$lib/user.json";
+  import portfolios from "$lib/portfolio.json";
 </script>
 
 <div class="text-center font-mono mt-14">
@@ -7,18 +7,18 @@
 </div>
 
 <div class="flex flex-col justify-center items-center gap-10 m-6 font-mono">
-  {#each users as user (user.id)}
+  {#each portfolios as port (user.id)}
     <div
       class="card w-11/12 xl:w-2/4 bg-base-100 border border-neutral hover:border-neutral-content ease-in-out transition-all duration-500 shadow-xl hover:bg-base-300"
     >
       <div class="card-body">
-        <h2 class="card-title text-3xl my-2">{user.name}</h2>
-        <img class="rounded border border-neutral" src={user.image} />
+        <h2 class="card-title text-3xl my-2">{port.name}</h2>
+        <img class="rounded border border-neutral" src={port.image} />
         <p class="my-2 bg-base-300 border border-neutral p-4 rounded">
-          {user.desc}
+          {port.desc}
         </p>
         <div class="card-actions justify-end">
-          <a href={user.url} class="btn btn-md font-mono border border-neutral"
+          <a href={port.url} class="btn btn-md font-mono border border-neutral"
             >Source Code</a
           >
         </div>
