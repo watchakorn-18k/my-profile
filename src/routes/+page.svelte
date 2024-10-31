be9b4570e5bd27f7b84f4075768fac1023ef457f
watchakorn-18k <wk18k@proton.me>
2024-10-31 15:44:54 +0700

    ✨ feat : add my reseme and button contact me

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import resume from "$lib/assets/my_resume.pdf";
   let hour: number = new Date().getHours();
   let minute: number = new Date().getMinutes();
   let second: number = new Date().getSeconds();
@@ -9,14 +10,23 @@
     minute = currentDate.getMinutes();
     second = currentDate.getSeconds();
   }, 1000);
+
+  const downloadFile = () => {
+    const link = document.createElement("a");
+    link.href = resume; // เปลี่ยนชื่อไฟล์และเส้นทางตามที่คุณต้องการ
+    link.download = "resume_watchakorn_buddeewong.pdf"; // ชื่อไฟล์ที่จะแสดงเมื่อดาวน์โหลด
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
 </script>
 
 <div class="bg-base-200 flex flex-col gap-2 items-center text-white">
   <div
-    class="hero min-h-screen bg-gradient-to-b from-base-300 to-base-100 from-[2rem] hover:to-primary font-mono"
+    class="hero min-h-screen bg-gradient-to-b from-base-300 to-base-100 from-[2rem] to-primary font-mono"
   >
     <div class="hero-content text-center">
-      <div class="max-w-md">
+      <div class="max-w-md flex flex-col gap-4">
         <div class="m-8">
           <span class="countdown font-mono text-4xl">
             <span style="--value:{hour}" />:
@@ -25,6 +35,10 @@
           </span>
         </div>
         <h1 class="text-2xl font-bold">👋Hello, I'm wk18k</h1>
+        <a href="mailto:wk18k@proton.me" class="btn btn-primary">Contact Me</a>
+        <button on:click={downloadFile} class="btn btn-neutral text-white"
+          >Download My Resume</button
+        >
       </div>
     </div>
   </div>
